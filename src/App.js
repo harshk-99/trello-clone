e053121641cb144ccbdf6154511fc908133e07db
harshk-99 <harsh.k9815@gmail.com>
2022-02-22 15:15:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./components/Board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App p-4"
+      style={{
+        background: "linear-gradient(to right, #0062cc, #007bff)",
+      }}
+    >
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      />
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,166 @@
+import { ReactSortable } from "react-sortablejs";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Board = () => {
+  const [ideas, setIdeas] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [published, setPublished] = useState([]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = async () => {
+    const task = await axios.get("http://localhost:1337/api/task");
+    const ideas = task.data.data.attributes.idea;
+
+    await axios
+      .put("http://localhost:1337/api/task", {
+        data: { idea: [...ideas, newTask] },
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error occured");
+      });
+    getTasks();
+  };
+
+  const getTasks = async () => {
+    let tasks = await axios.get("http://localhost:1337/api/task");
+    // For todos
+    let todos = tasks.data.data.attributes.todo;
+    setTodo(todos);
+    // For ideas
+    let ideas = tasks.data.data.attributes.idea;
+    setIdeas(ideas);
+    //For in progress
+    let inProgress = tasks.data.data.attributes.progress;
+    setInProgress(inProgress);
+    //published
+    let published = tasks.data.data.attributes.published;
+    setPublished(published);
+  };
+
+  const updateTasks = async () => {
+    const todosNew = todo.map((el) => String(el));
+    const ideasNew = ideas.map((el) => String(el));
+    const inProgressNew = inProgress.map((el) => String(el));
+    const publishedNew = published.map((el) => String(el));
+
+    await axios
+      .put("http://localhost:1337/api/task", {
+        data: {
+          idea: [...ideasNew].flat(),
+          todo: [...todosNew].flat(),
+          progress: [inProgressNew].flat(),
+          published: [publishedNew].flat(),
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error occured");
+      });
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  return (
+    <div className="container mt-5 mb-5">
+      <div
+        className="row"
+        style={{
+          height: "80vh",
+        }}
+      >
+        <div className="col mx-2 px-2 py-3 bg-light border rounded">
+          <h6>Idea</h6>
+          <div
+            style={{
+              minHeight: "500px",
+            }}
+          >
+            <ReactSortable
+              list={ideas}
+              setList={setIdeas}
+              group={{ name: "group-1", pull: true, put: true }}
+              onEnd={updateTasks}
+            >
+              {ideas.map((idea, index) => (
+                <div className="card p-3 border rounded mt-2" key={index + 1}>
+                  {idea}
+                </div>
+              ))}
+            </ReactSortable>
+          </div>
+          <div>
+            <textarea
+              rows={"1"}
+              cols={30}
+              style={{ float: "left", borderBlockColor: "#007bff" }}
+              value={newTask}
+              onChange={(event) => setNewTask(event.target.value)}
+            ></textarea>
+            <button
+              type="button"
+              style={{ float: "right", marginTop: "2px" }}
+              className="btn btn-primary btn-sm"
+              onClick={addTask}
+            >
+              Add Task
+            </button>
+          </div>
+        </div>
+        <div className="col mx-2 px-2 py-3 bg-light border rounded">
+          <h6>Todo</h6>
+
+          <ReactSortable
+            list={todo}
+            setList={setTodo}
+            group={{ name: "group-1", put: true, pull: true }}
+            onEnd={updateTasks}
+          >
+            {todo.map((todo, index) => (
+              <div className="card p-3 border rounded mt-2" key={index + 1}>
+                {todo}
+              </div>
+            ))}
+          </ReactSortable>
+        </div>
+        <div className="col mx-2 px-2 py-3 bg-light border rounded">
+          <h6>In Progress</h6>
+          <ReactSortable
+            list={inProgress}
+            setList={setInProgress}
+            group={{ name: "group-1", put: true, pull: true }}
+            onEnd={updateTasks}
+          >
+            {inProgress.map((inProgress, index) => (
+              <div className="card p-3 border rounded mt-2" key={index + 1}>
+                {inProgress}
+              </div>
+            ))}
+          </ReactSortable>
+        </div>
+        <div className="col mx-2 px-2 py-3 bg-light border rounded">
+          <h6>Published</h6>
+          <ReactSortable
+            list={published}
+            setList={setPublished}
+            group={{ name: "group-1", put: true, pull: true }}
+            onEnd={updateTasks}
+          >
+            {published.map((published, index) => (
+              <div className="card p-3 border rounded mt-2" key={index + 1}>
+                {published}
+              </div>
+            ))}
+          </ReactSortable>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
